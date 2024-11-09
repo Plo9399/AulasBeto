ee8290e66e9ec055f8cab6c5148cf09d906c23a8
Pedro Gonçalves <pedro.pessoal288@gmail.com>
2024-11-08 21:25:57 -0300

    atualizado

---
+++ aula2-1.py
@@ -0,0 +1,35 @@
+#estrutura condicional if-else
+
+#exemplo 1 
+#y = float(input(" digite o numero premiado deste sorteio: "))
+#x = float(input(" digite um numero para apostar a sorte: "))
+
+
+#if x == y:
+    #print("vc acertou!")
+    #print("vc acertou!")
+    #print("vc acertou!")
+    #print("vc acertou!")
+    #print("vc acertou!")
+
+#else:
+    #print("vc errou")
+    
+#print("----terminei de executar----")
+
+
+
+#exerc 3
+import random
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+
+if dado1 + dado2 == 7:
+    print("vc ganhou!")
+    print("vc ganhou!")
+    print("vc ganhou!")
+else:
+    print("vc não ganhou")
+    
+print("----terminei de executar----")
---
+++ aula2.py
@@ -0,0 +1,22 @@
+nome = input("digite seu nome aqui: ")
+idade = input("digite sua idade aqui: ")
+
+print("O nome digitado foi " + nome + "\n E sua idade é : " + idade)
+
+print(" O nome digitado foi {} e sua idade é {}".format(nome,idade))
+
+print(f'O nome que digitou é {nome} e sua idade é {idade}')
+
+#=============================
+# TECLA DE ATALHO PRA COPIAR A LINHA DE CIMA ( ALT + SHIFT + SETA PRA BAIXO)
+n1 = float(input(" digite um numero: "))
+
+n2 = int(input(" digite outro numero: "))
+
+
+print('essa é a soma dos numeros: ' , n1 + n2 )
+ 
+n3 = float(input(" digite o numeor de pi: "))
+
+n4 = float(input(" digite outro numero: "))
+print('essa é a soma dos numeros: ' , n3 + n4 )
